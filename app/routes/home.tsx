2b6cc2deb2a058e11d51c093af39e65957edba4e
manshaEI <vermamansha75@gmail.com>
2026-07-15 22:45:59 +0530

    feat: add puter store, pdf2img, resume components, and type definitions

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+// import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/navbar";
+import { resumes } from "../../constants";
+import { ResumeCard } from "../components/resumeCard"
+// import { callbackify } from "util";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return( 
+  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    
+    <section >
+     <div className="page-heading py-">
+      <h1>Track your application & resume ratings</h1>
+      <h2>Review your submissions and check AI-powered feedback.</h2>
+     </div>
+    
+    {resumes.length >0 &&(
+         <div className="resume-section">
+           {resumes.map( (resume) => (
+             <ResumeCard key={resume.id} resume={resume} />
+            ))}
+       
+          </div>
+    ) }
+    
+    </section>
+  </main>
+  );
 }
